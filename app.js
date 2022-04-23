326047642fa129f7706dad3d9b5270bbab97e2ac
8achi <shibaprasadpatra1999@gmail.com>
2022-04-24 01:54:34 +0530

    Now you can update data

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose');
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 mongoose.connect("mongodb://localhost:27017/employesDB");
 const employesSchema = new mongoose.Schema({
@@ -88,7 +89,7 @@ app.post("/addNewEmploye", function(req, res) {
 });
 
 app.post("/delete", function(req, res) {
-  var clickedEmployId = req.body.employeId;
+  const clickedEmployId = req.body.employeId;
   Employe.deleteMany({_id: clickedEmployId}, function(err) {
     if (err) {
       console.log(err);
@@ -101,30 +102,51 @@ console.log(clickedEmployId);
 
 });
 
-app.get("/:gettingPosition", function(req, res) {
-  const gettingPosition = req.params.gettingPosition;
-  Employe.find(function(err, employes) {
+app.get("/:gettingID", function(req, res) {
+  var gettingID = req.params.gettingID.toString();
+  console.log(gettingID);
 
-      res.render("edit", {
-        employeData: employes,
-        i: gettingPosition,
-      });
-    });
+Employe.findOne({_id: gettingID}, function (err, result) {
+    if (err){
+        console.log(err)
+    }
+    else{
+        console.log("Result : " + result);
+        console.log(result.name);
+        Employe.find(function(err, employes) {
+
+            res.render("edit", {
+              employeData: result,
+              id:gettingID,
+            });
+          });
+    }
 });
-// app.post("/edit", function(req, res) {
-//   // const editPersonData = new Employe({
-//   //   name: req.body.nameEdit,
-//   //   email: req.body.emailEdit,
-//   //   address: req.body.addressEdit,
-//   //   phone: req.body.phoneEdit,
-//   // });
-//
-// // console.log(editPersonData);
-// // console.log(editPersonData._id);
-//
-// // Employe.updateMany({name:"du"},{$set: {phone:9999999999}});
-//
-// });
+
+});
+app.post("/edit", function(req, res) {
+  const editPersonData = new Employe({
+    name: req.body.updateName,
+    email: req.body.updateEmail,
+    address: req.body.updateAddress,
+    phone: req.body.updatePhone,
+
+  });
+const id = req.body._id.toString()
+   Employe.updateOne({_id: id}, {name:editPersonData.name, email: editPersonData.email, address: editPersonData.address, phone: editPersonData.phone}, function(err){
+   if(err){
+     console.log(err);
+   }
+   else{
+     console.log("Updated succesfully")
+   }
+ });
+
+console.log(editPersonData);
+console.log(id);
+res.redirect("/");
+
+});
 
 
 
