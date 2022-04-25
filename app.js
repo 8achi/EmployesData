3c72d649f4230dfbe81bf6c4889fd0a8e74be9b2
8achi <shibaprasadpatra1999@gmail.com>
2022-04-25 11:07:03 +0530

    final stage

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
-var listOfID = [];
+// var listOfID = [];
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
 mongoose.connect("mongodb://localhost:27017/employesDB");
@@ -18,57 +20,29 @@ const employesSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    required: true
   },
   address: {
     type: String,
+    required: true
   },
   phone: {
-    type: String,
+    type: Number,
+    required: true,
+
   },
 });
 const Employe = mongoose.model("Employe", employesSchema);
-const employe1 = new Employe({
-  // sl: 1,
-  name: "Rahul Patra",
-  email: "rahul.patra@gmail.com",
-  address: "Kolkata,pin-700021,West Bengal",
-  phone: "+91 9679606243",
-
-});
-const employe2 = new Employe({
-  // sl: 2,
-  name: "Sid Patra",
-  email: "sid.patra@gmail.com",
-  address: "Bankura",
-  phone: "+91 967979999",
-
-});
-const employeList = [employe1, employe2];
-
-
 
 app.get("/", function(req, res) {
   Employe.find(function(err, employes) {
-    if (employes.length === 0) {
-      Employe.insertMany(employeList, function(err) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("add Successfully");
-        }
-        res.redirect("/");
-      });
-
-    } else {
-
-      res.render("home", {
-        employeData: employes
-      });
-    }
-
+    res.render("home", {
+      employeData: employes
+    });
   });
 
 });
+
 
 app.get("/addNewEmploye", function(req, res) {
   res.render("addNewEmploye", );
@@ -86,42 +60,48 @@ app.post("/addNewEmploye", function(req, res) {
 
 app.post("/delete", function(req, res) {
   const clickedEmployId = req.body.employeId;
-  Employe.deleteMany({_id: clickedEmployId}, function(err) {
+  Employe.deleteMany({
+    _id: clickedEmployId
+  }, function(err) {
     if (err) {
       console.log(err);
     } else {
       console.log("successfully delete")
     }
   });
-console.log(clickedEmployId);
+  console.log(clickedEmployId);
   res.redirect("/");
 
 });
 
 app.post("/delete_multiple", function(req, res) {
-  var check = req.body.ans;
-  var idForMultipleDelete = req.body.deleteMultiple;
-  listOfID.push(idForMultipleDelete);
+  var check = 'true';
+  var idForMultipleDelete  = req.body.dltArr;
+  // listOfID.push(idForMultipleDelete);
+  console.log(idForMultipleDelete);
 
-    if (check === 'true'){
-      for(var j = 0; j < listOfID.length; j++){
+  if (check === 'true') {
+    for (var j = 0; j < idForMultipleDelete.length; j++) {
 
-      Employe.deleteOne({_id: listOfID[j]}, function(err, docs) {
+
+      Employe.deleteOne({
+        _id: idForMultipleDelete[j]
+      }, function(err, docs) {
         if (err) {
-          console.log("something went wrong"+err);
-        } else
-        {
-          console.log(listOfID);
+          console.log("something went wrong" + err);
+        } else {
+          console.log(idForMultipleDelete);
           console.log("successfully delete")
 
         }
       });
       console.log("mission succesfully");
     }
-    res.redirect("/");
+    res.status(err.status || 200).json({status: err.status, message: err.message})
 
 
-}
+
+  }
 
 
 
@@ -131,22 +111,23 @@ app.get("/:gettingID", function(req, res) {
   var gettingID = req.params.gettingID.toString();
   console.log(gettingID);
 
-Employe.findOne({_id: gettingID}, function (err, result) {
-    if (err){
-        console.log(err)
-    }
-    else{
-        console.log("Result : " + result);
-        console.log(result.name);
-        Employe.find(function(err, employes) {
+  Employe.findOne({
+    _id: gettingID
+  }, function(err, result) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("Result : " + result);
+      console.log(result.name);
+      Employe.find(function(err, employes) {
 
-            res.render("edit", {
-              employeData: result,
-              id:gettingID,
-            });
-          });
+        res.render("edit", {
+          employeData: result,
+          id: gettingID,
+        });
+      });
     }
-});
+  });
 
 });
 app.post("/edit", function(req, res) {
@@ -157,21 +138,31 @@ app.post("/edit", function(req, res) {
     phone: req.body.updatePhone,
 
   });
-const id = req.body._id.toString()
-   Employe.updateOne({_id: id}, {name:editPersonData.name, email: editPersonData.email, address: editPersonData.address, phone: editPersonData.phone}, function(err){
-   if(err){
-     console.log(err);
-   }
-   else{
-     console.log("Updated succesfully")
-   }
- });
+  const id = req.body._id.toString()
+  Employe.updateOne({
+    _id: id
+  }, {
+    name: editPersonData.name,
+    email: editPersonData.email,
+    address: editPersonData.address,
+    phone: editPersonData.phone
+  }, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Updated succesfully")
+    }
+  });
 
-console.log(editPersonData);
-console.log(id);
-res.redirect("/");
+  console.log(editPersonData);
+  console.log(id);
+  res.redirect("/");
 
 });
+
+app.post('/cancel', function(req, res) {
+  res.redirect("/");
+})
 
 
 
